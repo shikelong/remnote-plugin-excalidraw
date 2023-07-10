b6da88f32d1d0afe7715adf351ae485c5f0961e6
Shi Kelong <578622705@qq.com>
2023-07-11 00:05:54 +0800

    (features) adding custom menu items (WIP)

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This plugin allows you to embed an Excalidraw board in RemNote. You can create E
 
 ## 📝 TODO
 - [ ] `Known issue` 🚨: Excalidraw Library are currently not supported. The page will be redirected to localhost:8000, so the Library cannot be added to the board.
+- [ ] `Known issue` 🚨: Image data is broken after restore.
 - [ ] `MVP` Design the UI to remove board & Set mode config.
 - [ ] `MVP` Enhance configuration: Add support for themes, height, view mode, zen mode etc. ⚙️
 - [ ] `MVP` Implement full-screen mode support. 🖥️
--- src/components/ExcalidrawBoard.tsx
+++ src/components/ExcalidrawBoard.tsx
@@ -1,13 +1,15 @@
-import { Excalidraw, WelcomeScreen } from '@excalidraw/excalidraw';
+import { Excalidraw, Footer, MainMenu, WelcomeScreen } from '@excalidraw/excalidraw';
 import { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types';
 import { ExcalidrawImperativeAPI, AppState, BinaryFiles } from '@excalidraw/excalidraw/types/types';
 import debounce from 'debounce';
 import deepEqual from 'deep-equal';
 import { memo, useState, useCallback, useEffect } from 'react';
 import { useStoredData } from '../hooks';
+import { usePlugin } from '@remnote/plugin-sdk';
 
 export const ExcalidrawBoard = memo(({ remId }: { remId?: string }) => {
   const [initialValue, setSyncedValue] = useStoredData(remId);
+  const plugin = usePlugin();
 
   const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null);
 
@@ -29,6 +31,33 @@ export const ExcalidrawBoard = memo(({ remId }: { remId?: string }) => {
   return (
     <Excalidraw onChange={handleChange} initialData={initialValue} ref={setRef}>
       <WelcomeScreen />
+      <MainMenu>
+        <MainMenu.Group>
+          <MainMenu.DefaultItems.LoadScene />
+          <MainMenu.DefaultItems.Export />
+          <MainMenu.DefaultItems.SaveAsImage />
+          <MainMenu.DefaultItems.ClearCanvas />
+        </MainMenu.Group>
+        <MainMenu.Separator></MainMenu.Separator>
+        <MainMenu.Group>
+          <MainMenu.DefaultItems.ToggleTheme />
+          <MainMenu.Item
+            onSelect={() => window.alert('Item1')}
+            icon={
+              <img className="-ml-1" src={`${plugin.rootURL}view.svg`} height={20} width={20} />
+            }
+          >
+            View mode
+          </MainMenu.Item>
+          <MainMenu.Item
+            onSelect={() => window.alert('Item1')}
+            icon={<img src={`${plugin.rootURL}zen.svg`} height={18} width={18} />}
+          >
+            Zen Mode
+          </MainMenu.Item>
+          <MainMenu.DefaultItems.ChangeCanvasBackground />
+        </MainMenu.Group>
+      </MainMenu>
     </Excalidraw>
   );
 }, deepEqual);
